f16b2ea49aed6edecce81cb4db07defeaf4576c5
romac <romac@users.noreply.github.com>
2022-03-23 10:10:21 +0000

    deploy: informalsystems/ibc-rs@43e5ccf44945c6bf989236d709a051dc5e997f80

---
+++ ibc_relayer/chain/cosmos/account/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"fn":[["query_account","Uses the GRPC client to retrieve the account sequence"]],"struct":[["Account","Wrapper for account number and sequence number."],["AccountNumber","Newtype for account numbers"],["AccountSequence","Newtype for account sequence numbers"]]});
