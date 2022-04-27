bf2a4277753c1d429a5b37302893393638453901
romac <romac@users.noreply.github.com>
2022-04-27 10:24:51 +0000

    deploy: informalsystems/ibc-rs@f7cbe1cfea7f4b7aeb1bf0e99a1791f46abb2e89

--- ibc_test_framework/relayer/channel/sidebar-items.js
+++ ibc_test_framework/relayer/channel/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"fn":[["assert_eventually_channel_established",""],["init_channel",""],["query_channel_end",""]],"trait":[["TaggedChannelEndExt",""]]});
+initSidebarItems({"fn":[["assert_eventually_channel_established",""],["init_channel",""],["query_channel_end",""],["query_identified_channel_end",""]],"trait":[["TaggedChannelEndExt",""]]});
