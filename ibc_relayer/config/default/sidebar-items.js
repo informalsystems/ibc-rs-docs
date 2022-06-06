acc097b0feb418043cd97006a8d73567d041209a
adizere <adizere@users.noreply.github.com>
2022-06-06 07:56:34 +0000

    deploy: informalsystems/ibc-rs@08b7f9884a3deb6e99332272fe442d620d5a3506

--- ibc_relayer/config/default/sidebar-items.js
+++ ibc_relayer/config/default/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"fn":[["clear_packets_interval",""],["clock_drift",""],["connection_delay",""],["max_block_time",""],["rpc_timeout",""],["tx_confirmation",""]]});
+initSidebarItems({"fn":[["chain_type",""],["clear_packets_interval",""],["clock_drift",""],["connection_delay",""],["max_block_time",""],["rpc_timeout",""],["tx_confirmation",""]]});
